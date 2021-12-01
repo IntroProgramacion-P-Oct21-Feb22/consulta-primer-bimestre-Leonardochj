997ae197998d1a966ae6d5883bf4299d6963dc04
Leonardo Chuquimarca <hlchuquimarca@utpl.edu.ec>
2021-11-30 19:12:02 -0500

    primer commit

--- ejemplo01.py
+++ ejemplo01.py
@@ -1,2 +1,13 @@
-mensaje = "¡hola mundo!"
-print(mensaje)
+name = input("¿Cómo te llamas? ")
+gender = input("¿Cuál es tu sexo (M o H)? ")
+if gender == "M":
+    if name.lower() < "m":
+        group = "A"
+    else:
+        group = "B"
+else:
+    if name.lower() > "n":
+        group = "A"
+    else:
+        group = "B"
+print("Tu grupo es " + group)
--- ejemplo02.py
+++ ejemplo02.py
@@ -1,2 +1,6 @@
-nombre = input("Introduce tu nombre: ")
-print("¡Hola " + nombre + "!")
+age = int(input("¿Cuál es tu edad? "))
+income = float(input("¿Cuales son tus ingresos mensuales?"))
+if age > 16 and income >= 1000:
+    print("Tienes que cotizar")
+else:
+    print("No tienes que cotizar")
